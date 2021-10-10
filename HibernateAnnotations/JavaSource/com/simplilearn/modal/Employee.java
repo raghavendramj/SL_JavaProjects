431d5cf710f5153b4ab10c053f79a0f4815da33f
raghavendramj <raghavendramj123@gmail.com>
2021-10-10 23:45:48 +0530

    HibernateAnnotations
    HibernateAnnotations

---
+++ HibernateAnnotations/JavaSource/com/simplilearn/modal/Employee.java
@@ -0,0 +1,82 @@
+package com.simplilearn.modal;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "Employee")
+public class Employee {
+
+	@Id
+	@GeneratedValue
+	@Column(name = "ID")
+	private Long id;
+
+	@Column(name = "username")
+	private String username;
+
+	@Column(name = "password")
+	private String password;
+
+	@Column(name = "email")
+	private String email;
+
+	@Column(name = "country")
+	private String country;
+
+	public Employee() {
+	}
+
+	public Employee(Long id, String username, String password, String email, String country) {
+		super();
+		this.id = id;
+		this.username = username;
+		this.password = password;
+		this.email = email;
+		this.country = country;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getCountry() {
+		return country;
+	}
+
+	public void setCountry(String country) {
+		this.country = country;
+	}
+
+}
---
+++ HibernateAnnotations/JavaSource/com/simplilearn/modal/Product.java
@@ -0,0 +1,74 @@
+package com.simplilearn.modal;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "Product")
+public class Product {
+
+	@Id
+	@GeneratedValue
+	@Column(name = "ID")
+
+	private long id;
+
+	@Column(name = "name")
+	private String name;
+
+	@Column(name = "price")
+	private BigDecimal price;
+
+	@Column(name = "date_added")
+	private Date date_added;
+
+	public Product() {
+	}
+
+	public Product(long id, String name, BigDecimal price, Date date_added) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.price = price;
+		this.date_added = date_added;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public BigDecimal getPrice() {
+		return price;
+	}
+
+	public void setPrice(BigDecimal price) {
+		this.price = price;
+	}
+
+	public Date getDate_added() {
+		return date_added;
+	}
+
+	public void setDate_added(Date date_added) {
+		this.date_added = date_added;
+	}
+
+}
