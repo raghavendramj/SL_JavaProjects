33c8f1dcfdc530f3df991d4ab03344718a1daaf1
raghavendramj <raghavendramj123@gmail.com>
2021-11-14 22:41:27 +0530

    Spring MVC Configurations
    Spring MVC Configurations

---
+++ spring-mvc-example/src/main/java/com/simplilearn/spring/model/Product.java
@@ -0,0 +1,48 @@
+package com.simplilearn.spring.model;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+public class Product {
+
+	private int id;
+	private String name;
+	private BigDecimal price;
+	private Date date;
+
+	public Product() {
+
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public BigDecimal getPrice() {
+		return price;
+	}
+
+	public void setPrice(BigDecimal price) {
+		this.price = price;
+	}
+
+	public Date getDate() {
+		return date;
+	}
+
+	public void setDate(Date date) {
+		this.date = date;
+	}
+}
